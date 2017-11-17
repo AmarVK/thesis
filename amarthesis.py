6487ea81142611fe867d5b5987e6ac9dbb9994b6
AmarVK <vamsi111124@gmail.com>
2017-11-17 09:49:23 -0500

    adding latest game file

--- amarthesis.py
+++ amarthesis.py
@@ -1,7 +1,7 @@
 #	ENSE 799 - Master's thesis
 #	Amar Vamsi Krishna - UID : 114921871
 #	University of Maryland, College Park
-#	Date : 25th October, 2017
+#	Date : 13th November, 2017
 #	Advisor : Dr. Anindo Roy
 #	Topic : Development of a novel interactive visual task for robot assisted gait training in stroke
 #	Task 2.1: Programming - Create a Video game - create a screen cursor and visual environment
@@ -9,136 +9,237 @@
 #	Configuration and Dependancies - Uses python version2.7.12 and pygame version 1.9.3. Currently using Spyder IDE and Sublime Text editor for development
 #	Source for the tutorial : https://pythonprogramming.net/pygame-python-3-part-1-intro/
 
-import pygame	
+import pygame, eztext
+import xlrd	
 
 pygame.init()										#	Initialize all imported pygame - modules (pygame constructor) 
 
-#gameDisplay = pygame.display.set_mode((800,600))	#	Initialize a window or screen for display
-screen = pygame.display.set_mode((800,600))
-black = [0, 0, 0]
-white = [255, 255, 255]
-yellow = [255, 255, 0]
-red = [255, 0, 0]
-screencolor = black
-screen.fill(screencolor)
-pygame.display.set_caption('Game On!')				#	Set the current window caption
-clock = pygame.time.Clock()							#	Create an object - 'clock' to help track time
+screen = pygame.display.set_mode((800,600))             #	 Initialize a window or screen for display
+black = [0, 0, 0]                                       #   Defining Black color for game rudiments
+white = [255, 255, 255]                                 #   Defining White color for game rudiments
+green = [0, 150, 0]                                     #   Defining Green color for game rudiments
+bright_green = [0, 255, 0]                              #   Defining bright Green color for game rudiments
+red = [150, 0 ,0]                                       #   Defining Red color for game rudiments
+bright_red = [255, 0 ,0]                                #   Defining brigh Red color for game rudiments
+screencolor = black                                     #   Defining screen color as black
+pygame.display.set_caption('Game On!')      				#   Set the current window caption
+clock = pygame.time.Clock()						           	#	Create an object - 'clock' to help track time
 
-#   Football field Background
-fieldimg = pygame.image.load('./data/field.png')
-fieldimg = pygame.transform.scale(fieldimg, (716, 500))
-
-# Goal image
-goalimg = pygame.image.load('./data/goal.png')
-
+#   Football field image
+fieldimg = pygame.image.load('./data/field.png')               #   load image from data folder
+fieldimg = pygame.transform.scale(fieldimg, (716, 500))        #   scale image by pixels
+startlineimg = pygame.image.load('./data/startline.png')
+startlineimg = pygame.transform.scale(startlineimg,(716,76))
 #Ball image
 ballimg = pygame.image.load('./data/ball.png')
 ballimg = pygame.transform.scale(ballimg, (50, 50))
+# Goal image
+goalimg = pygame.image.load('./data/goal.png')
 
-def field(x,y):
+def display_rudiments(x,y,linex,liney,x1,y1,x2,y2):
     screen.blit(fieldimg, (x,y))
-x =  42
-y = 50
-
-def goal(x2,y2):
-    screen.blit(goalimg, (x2,y2))
-x2 =  300
-y2 =  490
-
-def ball(x1,y1):
+    screen.blit(startlineimg, (linex,liney))
+    linew = 716
+    lineh = 5
+    startline(linex, liney, linew, lineh)
     screen.blit(ballimg, (x1,y1))
-x1 = 376
-y1 = 50
-y_change = 0.0
-y_end = 0.0
-y_step = 10
+    screen.blit(goalimg, (x2,y2))
 
-def bar(barx, bary, barw, barh, color):
-    pygame.draw.rect(screen, color, [barx, bary, barw, barh])
-bar_startx = 8
-bar_starty = 8
-bar_width = 20
-bar_height = 2
-bar_hchange = 50
-bar_max = 0
+#   Defining bar as an object with multiple inputs
+def bar(barx, bary, barw, barh):
+    pygame.draw.rect(screen, white, [barx-2, bary-2, barw+4, 404])
+    pygame.draw.rect(screen, black, [barx, bary, barw, 400])
+    pygame.draw.rect(screen, white, [barx, bary, barw, barh])
+    
+#   Defining start-line as an object with multiple inputs
+def startline(linex, liney, linew, lineh):
+    pygame.draw.rect(screen, red, [linex, liney, linew, lineh])
 
-def kickpower(power_count):
+#   Defining kickpower as an object with power input to display the kick power on the top right corner of the window
+def kickpower(power):
     font = pygame.font.Font('./data/font.ttf', 40)
     kickmeter = font.render('Kickmeter',1,white)
-    text = font.render("Power: "+str(power_count)+ "%", True, white)
-    screen.blit(text,(600,10))
+    text = font.render("Power: "+str(power)+ "%", True, white)
+    screen.blit(text,(590,10))
     screen.blit(kickmeter,(30,10))
-power_count = 0.00
 
-crashed = False										#	Initialize the boolean - 'crashed' to false 	
+#   Extracting torque data from the excel file
+file_location = "./data/Robot_data1.xlsx"
+workbook = xlrd.open_workbook(file_location)
+sheet = workbook.sheet_by_name('Trial 1')
+#   Extracting Column 7 from the sheet
+torque = sheet.col_values(6)
+footswitch = sheet.col_values(19)
 
-#	The game loop logic runs as long as there is no crash
+#   Extracting meaningful values of torque
+tlength = len(torque)
+for i in range(0,tlength):
+    if torque[i] > 0:
+        torque[i] = 0.00
+    else:
+        torque[i] = (-1)*torque[i]
+for i in range(0,tlength):
+    if footswitch[i] < 0.3:
+        torque[i] = 0
+        footswitch[i] = 0
 
-while not crashed:									#	The game loop begins
+
+action = "None"	
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def button(msg,x,y,w,h,ic,ac,action):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, ac,(x,y,w,h))
+        if click[0] == 1 and action != "None":
+            if action == "play":
+                game_loop()
+            elif action == "quit":
+                pygame.quit()
+                quit()
+    else:
+        pygame.draw.rect(screen, ic,(x,y,w,h))
+
+    smallText = pygame.font.Font('./data/font.ttf',20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    screen.blit(textSurf, textRect)
+    
+def game_intro():
+
+    largeText = pygame.font.Font('./data/font.ttf',115)
+    TextSurf, TextRect = text_objects("Game On!", largeText)
+    TextRect.center = ((800/2),100)
+    smallText = pygame.font.Font('./data/font.ttf',40)
+    txtbx1 = eztext.Input(x=200, y=250, font=smallText, maxlength=5, color=(0,0,0), restricted = '1234567890.', prompt='Start difficulty: ')
+    txtbx2 = eztext.Input(x=200, y=300, font=smallText, maxlength=5, color=(0,0,0), restricted = '1234567890.', prompt='Torque sensitivity: ')
+    txtbx3 = eztext.Input(x=200, y=350, font=smallText, maxlength=5, color=(0,0,0), restricted = '1234567890.', prompt='Progression: ')
+    intro = True
+
+    while intro:
+        events = pygame.event.get()
+        for event in events:
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        screen.fill(white)
+        # update txtbx
+        txtbx1.update(events)
+        txtbx2.update(events)
+        txtbx3.update(events)
+        screen.blit(TextSurf, TextRect)
+        txtbx1.draw(screen)
+        txtbx2.draw(screen)
+        txtbx3.draw(screen)
+
+        button("Start",150,450,100,50,green,bright_green,"play")
+        button("Exit",550,450,100,50,red,bright_red,"quit")    
+
+        pygame.display.update()
+        clock.tick(15)
         
-    for event in pygame.event.get():				#	Gets all the events from the game
+#	The game loop logic runs as long as there is no crash
+def game_loop():
+    x =  42                 #   Field x position
+    y = 50                  #   Field y position
+    L = 250                 #   Initial Start position
+    Lmax = 250              #   Max start positiom
+    Lmin = 50               #   Min start position position
+    linex = x               #   Start-line x position (same as field)
+    liney = L               #   Initial Start-line y position (same as Start position)
+    x1 = 376                #   Initial Ball x position (center of the field)
+    y1 = L                  #   Initial Ball y position (same as Initial Start position)
+    y1actual = 0            #   Initial y value wrt torque profile
+    ymax = 488              #   Max y position of ball
+    x2 =  300               #   Net x position
+    y2 =  490               #   Net y position
+    alpha_sens = 41.66      #   Sensitivity from dialog box (pixels/Nm)
+    barx = 8                #   Power bar x position   
+    bary = 8                #   Power bar y position
+    barw = 20               #   Power bar width in pixels   
+    barh = 2                #   Power bar height in pixels
+    power = 0.00            #   Percentage of human torque wrt total torque
+    count = 0               #   Used for indexing data from .xlsx file
+    read_value = 0          #   Initializing current torque value
+    prev_value = 0          #   Initializing max torque value in the current gait cycle
+    gait_cycle = 0          #   Initializing gait_cycle count
+    print(gait_cycle+1)
+    sample_size = 5         #   Setting the window size for machine learning
+    max_torque = [0]*sample_size            #   Initializing an empty list of length= sample_size to store max torque values of every iteration
+    flag = True             #   Used to detect change in gait_cycle
+    average_torque = 0      # Initializing average torque value for machine learning
+    crashed = False         #	Initialize the boolean - 'crashed' to false
+    while not crashed:									#	The game loop begins
+            
+        for event in pygame.event.get():				#	Gets all the events from the game
+            if event.type == pygame.QUIT:			#	If the event is 'pygame.QUIT' - pressing of the 'X' button on the display window, the boolean crashed is flagged to a true value
+                crashed = True	
+                pygame.quit()
+                quit()
+                
+            if count == tlength:
+                crashed = True
+                pygame.quit()
+                quit()
+                
+        read_value = torque[count]
+        count += 1
+        if flag == False:           #   Swing phase: no ball movement
+            prev_value = 0          #   New gait_cycle detected; set max torque value to zero
+            if footswitch[count] != 0:          #   Wait for stance phase 
+                flag = True
+        elif flag == True:          #   Stance phase
+            if read_value>prev_value:
+                prev_value = read_value
+            if footswitch[count] == 0:          #   New gait cycle detected
+                flag = False
+                gait_cycle += 1
+                print(max_torque)
+                if gait_cycle < sample_size: print(gait_cycle+1)
+            if gait_cycle < sample_size:
+                max_torque[gait_cycle] = prev_value            #   save max_torque for gait_cycle < sample size
+            else:
+                #   Compare last sample with the average of the rest and decide improvement and declining
+                average_torque = sum(max_torque[:sample_size-1])/(sample_size-1)
+                if average_torque < max_torque[sample_size-1]:
+                    print('Improvement')
+                    L = L - 50
+                    if L < Lmin:
+                        L = Lmin
+                elif average_torque > max_torque[sample_size-1]:
+                    print('Decline')
+                    L = L + 50
+                    if L > Lmax:
+                        L = Lmax
+                elif average_torque == max_torque[sample_size-1]:
+                    L = L
+                gait_cycle = 0                      #   Reset gait_cycle
+                print(gait_cycle+1)
+                max_torque = [0]*sample_size        #   Reset max_torque
+        #   Update y position of ball & start line and power bar height
+        y1 = L + alpha_sens*prev_value
+        y1actual = L + alpha_sens*read_value
+        if y1 > ymax:
+            y1 = ymax
+        if y1actual > ymax:
+            y1actual = ymax
+        liney = L
+        barh = 400*(y1actual-L)/(ymax-L)
+        power = 100*(y1actual-L)/(ymax-L)
         screen.fill(screencolor)
-        field(x,y)        						
-        ball(x1,y1)
-        goal(x2,y2)
-        bar(bar_startx, bar_starty, bar_width, bar_height, white)
-        kickpower(power_count)
-        if event.type == pygame.QUIT:				#	If the event is 'pygame.QUIT' - pressing of the 'X' button on the display window, the boolean crashed is flagged to a true value
-            crashed = True	
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                y_change = 1.0
-            elif event.key == pygame.K_2:
-                y_change = 2.0
-            elif event.key == pygame.K_3:
-                y_change = 3.0
-            elif event.key == pygame.K_4:
-                y_change = 4.0
-            elif event.key == pygame.K_5:
-                y_change = 5.0
-            elif event.key == pygame.K_6:
-                y_change = 6.0
-            elif event.key == pygame.K_7:
-                y_change = 7.0
-            elif event.key == pygame.K_8:
-                y_change = 8.0
-            elif event.key == pygame.K_9:
-                y_change = 9.0
-            elif event.key == pygame.K_0:
-                y_change = 10.0
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_1 or event.key == pygame.K_2 or event.key == pygame.K_3 or event.key == pygame.K_4 or event.key == pygame.K_5 or event.key == pygame.K_6 or event.key == pygame.K_7 or event.key == pygame.K_8 or event.key == pygame.K_9 or event.key == pygame.K_0:
-                 power_count = y_change*10
-                 y_end = 475*(y_change/10)
-                 bar_max = 500*(y_change/10)
-                 while (y1 < y_end):
-                     y1 += y_step
-                     bar_height += bar_hchange
-                     if bar_height > bar_max:
-                         bar_hchange = 0
-                     screen.fill(screencolor)
-                     field(x,y)        						
-                     ball(x1,y1)
-                     goal(x2,y2)
-                     bar(bar_startx, bar_starty, bar_width, bar_height, white)
-                     kickpower(power_count)
-                     pygame.display.update()						#	Updates the display screen only in the places where the event has changed
-                     clock.tick(60)								#	Max framerate of the game
-                 pygame.time.delay(200)
-                 y_change = 0.0
-                 bar_height = 2
-                 bar_hchange = 20
-                 bar_max = 0
-                 y1 = 33
-                 screen.fill(screencolor)
-                 field(x,y)        						
-                 ball(x1,y1)
-                 goal(x2,y2)
-                 bar(bar_startx, bar_starty, bar_width, bar_height, white)
-                 kickpower(power_count)
-    print (event) 								#	Displays the event log on the terminal screen
-    pygame.display.update()						#	Updates the display screen only in the places where the event has changed
-    clock.tick(60)								#	Max framerate of the game
-
-pygame.quit()										#	Unitialize all pygame modules (pygame destructor)
-quit()												#	Quit Python
+        display_rudiments(x,y,linex,liney,x1,y1,x2,y2)      						
+        bar(barx, bary, barw, barh)
+        kickpower(power)
+        pygame.display.update()						#	Updates the display screen only in the places where the event has changed
+        clock.tick(200)								#	Max framerate of the game
+    
+    pygame.quit()										#	Unitialize all pygame modules (pygame destructor)
+    quit()												#	Quit Python
+game_intro()
