0cf131289990a7fcf1a36652a17f4fde1c486b04
AmarVK <vamsi111124@gmail.com>
2017-11-17 14:39:53 -0500

    Stashing

--- main-menu.py
+++ main-menu.py
@@ -20,22 +20,24 @@ FONT = ("Verdana",10)
 # =============================================================================
 window = Tk()                                        #   Creating a Tkinter object
 window.title("Penalty Shooter")                         #   Creating a title
-window.geometry("600x420+0+0")                          #   Creating a screen x -> the dimensions and +gives the initial position location 
+window.geometry("500x420+0+0")                          #   Creating a screen x -> the dimensions and +gives the initial position location 
 # window.wm_iconbitmap('ball.xbm')                      #   TODO Fix this
 # =============================================================================
 # Define text and photo inputs
 # =============================================================================
 logo = PhotoImage(file='/home/amarvk/projects/thesis/images/fire-soccer.gif')
 header_text = """Penalty Shooter Game !
-A game to engage the plantarflexion movement 
-of the stroke patient using soccer"""
+Robot Assisted Soccer Game"""
 menu_heading = """ Please enter the game parameters"""
-input1 = """Sensitivity - α"""
-input2 = """Threshold torque - τ"""
-input3 = """Level Progression parameter"""
+input1 = """Sensitivity (pixels/Nm)"""
+input2 = """Threshold (Nm)"""
+input3 = """Default Pixel Offset"""
+input4 = """Machine Learning"""
 checkbox1 = """None"""
 checkbox2 = """Average"""
-checkbox3 = """Maximum"""
+# =============================================================================
+# checkbox3 = """Maximum"""
+# =============================================================================
 checkbox4 = """Regression"""
 game_button = """Go to Game !"""
 blanker = """-------------------------------------------------------------------------------------------"""
@@ -51,7 +53,9 @@ footer_image = PhotoImage(file='/home/amarvk/projects/thesis/images/umd.gif')
 # =============================================================================
 introText = Label(window,text = header_text, justify = LEFT, font = HEADER_FONT).grid(row=0, column =0, sticky=W)
 topLogo = Label(window, image = logo).grid(row =0, column = 1, sticky = W+E+N+S)
-menuHeading = Label(window,text = menu_heading, font = FONT).grid(row=1, sticky=W+E+N+S)
+# =============================================================================
+# menuHeading = Label(window,text = menu_heading, font = FONT).grid(row=1, sticky=W+E+N+S)
+# =============================================================================
 
 input1Text = Label(window,text = input1, font = FONT).grid(row=2, column =0, sticky=W)
 input1entry = Entry(window)
@@ -62,18 +66,24 @@ input2Text = Label(window,text = input2, font = FONT).grid(row=3, column =0, sti
 input2entry = Entry(window)
 input2entry.grid(row=3, column =1, sticky=W)
 
+input3Text = Label(window,text = input3, font = FONT).grid(row=4, column =0, sticky=W)
+input3entry = Entry(window)
+input3entry.grid(row=4, column =1, sticky=W)
+
 cb_val1 = IntVar()
 cb_val1.set(0)
-input3Text = Label(window,text = input3, font = FONT).grid(row=4, column =0, sticky=W)
-input3entry1 = Checkbutton(window, text=checkbox1, variable = cb_val1, onvalue =1, offvalue =0).grid(row=4, column =1, sticky =W)
+input3Text = Label(window,text = input4, font = FONT).grid(row=5, column =0, sticky=W)
+input3entry1 = Checkbutton(window, text=checkbox1, variable = cb_val1, onvalue =1, offvalue =0).grid(row=5, column =1, sticky =W)
 
 cb_val2 = IntVar()
 cb_val2.set(0)
-input3entry2 = Checkbutton(window, text=checkbox2, variable = cb_val2, onvalue =1, offvalue =0).grid(row=5, column =1, sticky =W)
-
-cb_val3 = IntVar()
-cb_val3.set(0)
-input3entry3 = Checkbutton(window, text=checkbox3, variable = cb_val3, onvalue =1, offvalue =0).grid(row=6, column =1, sticky =W)
+input3entry2 = Checkbutton(window, text=checkbox2, variable = cb_val2, onvalue =1, offvalue =0).grid(row=6, column =1, sticky =W)
+# =============================================================================
+# 
+# cb_val3 = IntVar()
+# cb_val3.set(0)
+# input3entry3 = Checkbutton(window, text=checkbox3, variable = cb_val3, onvalue =1, offvalue =0).grid(row=6, column =1, sticky =W)
+# =============================================================================
 
 cb_val4 = IntVar()
 cb_val4.set(0)
@@ -82,7 +92,9 @@ input3entry4 = Checkbutton(window, text=checkbox4, variable = cb_val4, onvalue =
 def gameAction():
     alpha = float(input1entry.get())
     tau = float(input2entry.get())
-    print cb_val1.get(), cb_val2.get(), cb_val3.get(), cb_val4.get()
+# =============================================================================
+#     print cb_val1.get(), cb_val2.get(), cb_val3.get(), cb_val4.get()
+# =============================================================================
     os.system('python amarthesis.py')
     
 gameButton = Button(window, text = game_button, command =lambda: gameAction()).grid (row = 8,column = 0, columnspan =2)
