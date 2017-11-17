aee27dbd9f097a6af7c5660e88014bea515c81c7
AmarVK <vamsi111124@gmail.com>
2017-11-17 04:54:39 -0500

    taking values from the menu

--- main-menu.py
+++ main-menu.py
@@ -9,13 +9,12 @@ Created on Thu Nov 16 13:59:27 2017
 # Import Modules
 # =============================================================================
 from Tkinter import *
-import tkFont
+import numpy as np
 # =============================================================================
 # Deffine Fonts
 # =============================================================================
 HEADER_FONT = ("Verdana",12)
 FONT = ("Verdana",10)
-# =============================================================================
 # Initialize window
 # =============================================================================
 window = Tk()                                        #   Creating a Tkinter object
@@ -37,6 +36,7 @@ checkbox1 = """None"""
 checkbox2 = """Average"""
 checkbox3 = """Maximum"""
 checkbox4 = """Regression"""
+game_button = """Go to Game !"""
 blanker = """-------------------------------------------------------------------------------------------"""
 team = """Team :"""
 team_members="""Amar Vamsi Krishna
@@ -53,33 +53,51 @@ topLogo = Label(window, image = logo).grid(row =0, column = 1, sticky = W+E+N+S)
 menuHeading = Label(window,text = menu_heading, font = FONT).grid(row=1, sticky=W+E+N+S)
 
 input1Text = Label(window,text = input1, font = FONT).grid(row=2, column =0, sticky=W)
-input1entry = Entry(window).grid(row=2, column =1, sticky=W)
+input1entry = Entry(window)
+input1entry.grid(row=2, column =1, sticky=W)
+
 
 input2Text = Label(window,text = input2, font = FONT).grid(row=3, column =0, sticky=W)
-input2entry = Entry(window).grid(row=3, column =1, sticky=W)
+input2entry = Entry(window)
+input2entry.grid(row=3, column =1, sticky=W)
 
+cb_val1 = IntVar()
+cb_val1.set(0)
 input3Text = Label(window,text = input3, font = FONT).grid(row=4, column =0, sticky=W)
-input3entry1 = Checkbutton(window, text=checkbox1).grid(row=4, column =1, sticky =W)
-input3entry2 = Checkbutton(window, text=checkbox2).grid(row=5, column =1, sticky =W)
-input3entry3 = Checkbutton(window, text=checkbox3).grid(row=6, column =1, sticky =W)
-input3entry4 = Checkbutton(window, text=checkbox4).grid(row=7, column =1, sticky =W)
+input3entry1 = Checkbutton(window, text=checkbox1, variable = cb_val1, onvalue =1, offvalue =0).grid(row=4, column =1, sticky =W)
+
+cb_val2 = IntVar()
+cb_val2.set(0)
+input3entry2 = Checkbutton(window, text=checkbox2, variable = cb_val2, onvalue =1, offvalue =0).grid(row=5, column =1, sticky =W)
+
+cb_val3 = IntVar()
+cb_val3.set(0)
+input3entry3 = Checkbutton(window, text=checkbox3, variable = cb_val3, onvalue =1, offvalue =0).grid(row=6, column =1, sticky =W)
+
+cb_val4 = IntVar()
+cb_val4.set(0)
+input3entry4 = Checkbutton(window, text=checkbox4, variable = cb_val4, onvalue =1, offvalue =0).grid(row=7, column =1, sticky =W)
+
+def gameAction():
+    alpha = float(input1entry.get())
+    tau = float(input2entry.get())
+    print cb_val1.get(), cb_val2.get(), cb_val3.get(), cb_val4.get()
+    
+gameButton = Button(window, text = game_button, command =lambda: gameAction()).grid (row = 8,column = 0, columnspan =2)
 # =============================================================================
 # Setting up the footer UI
 # =============================================================================
-divider = Label(window,text = blanker, font = FONT).grid(row=8, column = 0, columnspan =2, sticky = N+E+W+S)
-teamText = Label(window,text = team, font = FONT).grid(row=9, column = 0, columnspan =2, sticky = N+E+W+S)
-teamMembers = Label(window,text = team_members, font = FONT).grid(row=10, column = 0, columnspan =2, sticky = N+E+W+S)
-divider = Label(window,text = blanker, font = FONT).grid(row=11, column = 0, columnspan =2, sticky = N+E+W+S)
-advisorText = Label(window,text = advisor, font = FONT).grid(row=12, column = 0, columnspan =2, sticky = N+E+W+S)
-advisorName = Label(window,text = advisor_name, font = FONT).grid(row=13, column = 0, columnspan =2, sticky = N+E+W+S)
-bottomLogo = Label(window, image = footer_image).grid(row =14, column = 0, columnspan = 2, sticky = W+E+N+S)
-divider = Label(window,text = blanker, font = FONT).grid(row=15, column = 0, columnspan =2, sticky = N+E+W+S)
+divider = Label(window,text = blanker, font = FONT).grid(row=9, column = 0, columnspan =2, sticky = N+E+W+S)
+teamText = Label(window,text = team, font = FONT).grid(row=10, column = 0, columnspan =2, sticky = N+E+W+S)
+teamMembers = Label(window,text = team_members, font = FONT).grid(row=11, column = 0, columnspan =2, sticky = N+E+W+S)
+divider = Label(window,text = blanker, font = FONT).grid(row=12, column = 0, columnspan =2, sticky = N+E+W+S)
+advisorText = Label(window,text = advisor, font = FONT).grid(row=13, column = 0, columnspan =2, sticky = N+E+W+S)
+advisorName = Label(window,text = advisor_name, font = FONT).grid(row=14, column = 0, columnspan =2, sticky = N+E+W+S)
+bottomLogo = Label(window, image = footer_image).grid(row =15, column = 0, columnspan = 2, sticky = W+E+N+S)
+divider = Label(window,text = blanker, font = FONT).grid(row=16, column = 0, columnspan =2, sticky = N+E+W+S)
 
-# =============================================================================
-# widget3 = Label(window, text="Sensitivity - alpha")
-# widget3.pack( side = LEFT)
-# widget4 = Entry(window, bd =5)
-# widget4.pack(side = RIGHT)
-# =============================================================================
 
 window.mainloop()                                       #   The window won't appear until we enter the Tkinter event loop: Our script will remain in the event loop until we close the window
+
+s = input1entry.get()
+print s
