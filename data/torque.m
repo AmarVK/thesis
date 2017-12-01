ad76742e6724ccb2636de64b5472a06243eb2e2d
suchitrachandar <32778958+suchitrachandar@users.noreply.github.com>
2017-11-30 21:50:35 -0500

    You are going to kill me!

--- data/torque.m
+++ data/torque.m
@@ -1,6 +1,6 @@
 clc
 clear all
-robot_data = xlsread('Robot_Data','Trial 1','A1:U2038','basic');
+robot_data = xlsread('Robot_Data1','Trial 1','A1:U9889','basic');
 %9889
 for i = 1: length(robot_data)
     if robot_data(i,7) > 0
@@ -16,6 +16,7 @@ for i = 1: length(robot_data)
         torque_data1(i) = torque_data(i);
     end
 end
+
 i = 1:length(robot_data);
 figure(1)
 plot(i,torque_data1,'r',i,robot_data(:,7),'b',i, robot_data(:,20),'k')
