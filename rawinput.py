74ebb05ca005bbfffa4b47d133a8c43b1e6b79fc
suchitrachandar <32778958+suchitrachandar@users.noreply.github.com>
2018-01-09 10:28:33 -0500

    Sample program

---
+++ rawinput.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 09 09:25:13 2018
+
+@author: Suchitra Chandar
+"""
+
+flag = 1
+count = 0
+torque_values = []
+footswitch_values = []
+while flag == 1:
+    footswitch = raw_input("Footswitch:  ")
+    torque = raw_input("Torque:  ")
+    footswitch_values.append(footswitch)
+    torque_values.append(torque)
+    count += 1
