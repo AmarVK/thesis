fd05b5720b1a9de59d55a881ef2bfa70f60f69ea
suchitrachandar <32778958+suchitrachandar@users.noreply.github.com>
2018-03-01 12:43:21 -0500

    Updated Matlab files

--- data/torque.m
+++ data/torque.m
@@ -1,30 +1,18 @@
 clc
 clear all
 close all
-robot_data = xlsread('trial3','sheet1','A1:U2389','basic');
-%9889
-% for i = 1: length(robot_data)
-%     if robot_data(i,7) > 0
-%         torque_data(i) = 0;
-%     else
-%         torque_data(i) = -robot_data(i,7);
-%     end
-% end
-% for i = 1: length(robot_data)
-%     if robot_data(i,20) < 0.3
-%         torque_data1(i) = 0;
-%     else
-%         torque_data1(i) = torque_data(i);
-%     end
-% end
-i = 1:length(robot_data);
+robot_data = xlsread('trial2','book','A1:U6389','basic');
+i =(1/200):(1/200):(length(robot_data)/200);
 figure(1)
-plot(i,10*robot_data(:,3),'r',i,robot_data(:,7),'b',i, robot_data(:,20),'k')
+grid on
+plot(i,10*robot_data(:,3),'r',i,robot_data(:,7),'b',i, robot_data(:,20),'k',i(2:length(robot_data)),smooth(20*diff(robot_data(:,5))),'g',i,robot_data(:,5),'m')
+xlabel('time (seconds)')
+legend('angle','torque','footswitch','predicted torque')
 figure(2)
-plot(robot_data(:,7),'b')
+plot(i(2:length(robot_data)),200*diff(robot_data(:,3)),'b',i,robot_data(:,5),'k')
 figure(3)
-plot(i,robot_data(:,3),'r')
+plot(i(2:length(robot_data)),200*smoothdata(diff(robot_data(:,5)),'gaussian',20),'k',i,robot_data(:,5),'r')
 figure(4)
-plot(diff(diff(robot_data(:,3))),'b')
+plot(diff(robot_data(:,5)),'b')
 figure(5)
 plot(i,robot_data(:,20),'b')
